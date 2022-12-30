ad72fac698c09175382276254d582a2078ea9acf
pmudassir <pmuddasir@gmail.com>
2022-12-30 19:35:11 +0530

    product page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
 
 const App = () => {
-  return <ProductList />;
+  return <Product />;
 };
 
 export default App;
