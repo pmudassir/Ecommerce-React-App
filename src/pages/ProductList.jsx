5775df7c5e443b8793c9396e44e40c0862c82802
pmudassir <pmuddasir@gmail.com>
2022-12-30 17:27:19 +0530

    products page created

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import styled from 'styled-components'
+import Navbar from '../components/Navbar'
+import Announcement from '../components/Announcement'
+import Products from '../components/Products'
+import Footer from '../components/Footer'
+import Newsletter from '../components/Newsletter'
+
+const Container = styled.div`
+
+`;
+
+const Title = styled.h1`
+    margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Filter = styled.div`
+    margin: 20px;
+`;
+
+const FilterText = styled.span`
+    font-size: 20px;
+    font-weight: 600;
+    margin-right: 20px;
+`;
+
+const Select = styled.select`
+    padding: 10px;
+    margin-right: 20px;
+`;
+
+const Option = styled.option`
+
+`;
+
+const ProductList = () => {
+    return (
+        <Container>
+            <Announcement />
+            <Navbar />
+            <Title>DRESSES</Title>
+            <FilterContainer>
+                <Filter>
+                    <FilterText>Filter Items:</FilterText>
+                    <Select>
+                        <Option disabled selected>
+                            Color
+                        </Option>
+                        <Option>White</Option>
+                        <Option>Black</Option>
+                        <Option>Red</Option>
+                        <Option>Blue</Option>
+                        <Option>Yellow</Option>
+                        <Option>Green</Option>
+                    </Select>
+                    <Select>
+                        <Option disabled selected>
+                            Size
+                        </Option>
+                        <Option>XS</Option>
+                        <Option>S</Option>
+                        <Option>M</Option>
+                        <Option>L</Option>
+                        <Option>XL</Option>
+                    </Select>
+                </Filter>
+                <Filter>
+                    <FilterText>Sort Items:</FilterText>
+                    <Select>
+                        <Option selected>Newest</Option>
+                        <Option>Price (asc)</Option>
+                        <Option>Price (desc)</Option>
+                    </Select>
+                </Filter>
+            </FilterContainer>
+            <Products />
+            <Newsletter />
+            <Footer />
+        </Container>
+    )
+}
+
+export default ProductList
